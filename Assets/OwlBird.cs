d9397eaca696edac55a7deef627d031ea3418036
Rakha Fadhilah <rakha.1243@gmail.com>
2021-02-09 15:58:00 +0700

    add src

---
+++ Assets/OwlBird.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    public bool _hasExplode = false;
+
+    public void Explode()
+    {
+        if (State == BirdState.HitSomething && !_hasExplode)
+        {
+            transform.localScale = new Vector2(2.2f, 2.2f);
+            _hasExplode = true;
+        }
+    }
+
+    public override void OnCollisionEnter2D(Collision2D col)
+    {
+        State = BirdState.HitSomething;
+        Explode();
+    }
+}
